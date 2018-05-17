43c4af27c1b64ef047792db83a4595d55dcb3de8
Gabriel <gabriel.padua.dev@gmail.com>
2018-05-17 04:54:22 -0300

    compose part 2

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,12 +2,31 @@ const express = require('express')
 const restful = require('node-restful')
 const server  = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
+// MIDDLEWARES
+server.use(bodyParser.urlencoded({extendend: true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+//ODM -- mapeamento objeto documento
+const Client = restful.model('Client', {
+    name: {type: String, require: true}
+})
+
+//Rest api
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
 //Teste
-server.get('/', (req, res, next) => res.send('Backoficce'))
+//server.get('/', (req, res, next) => res.send('Backoficce'))
 
 server.listen(3000)
